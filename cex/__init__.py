a22d4586c2f87b2ce1194522e7e74e7fc09a08cb
Kristen Gerring <KGerring@gmail.com>
2016-04-20 02:02:06 -0400

    created __init__

---
+++ cex/__init__.py
@@ -0,0 +1,11 @@
+"""cex - Python FCA Lattice Miner and Builder"""
+
+__version__ = '0.1.0'
+__author__ = 'Kristen Gerring <oursexploration@gmail.com>'
+__all__ = []
+import os
+DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
+
+path = '/Applications/PycharmProjects1/proj/cex/'
+
+
