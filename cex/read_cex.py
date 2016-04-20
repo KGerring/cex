c1fbe6ebd49bd1c269834906e891b4114e367405
Kristen Gerring <KGerring@gmail.com>
2016-04-20 02:02:43 -0400

    created read_cex

---
+++ cex/read_cex.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+#packages =,
+#date = 4/19/16find
+
+
+
+
+if __name__ == "__main__":
+	test_file = '/Applications/PycharmProjects1/proj/cex/data/PowerContext.cex'
